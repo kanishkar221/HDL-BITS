929c7badee2eceb59b29c33fb61c0ce95654cf8c
KANISHKA R <kanishka.r221@gmail.com>
2025-09-13 16:14:48 +0530

    Update 9. Adder Subtractor.v

--- Verilog Language/Modules/9. Adder Subtractor.v
+++ Verilog Language/Modules/9. Adder Subtractor.v
@@ -1,1 +1,13 @@
-
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    input sub,
+    output [31:0] sum
+);
+	wire w1;
+    wire [31:0]b_xor;
+    
+    assign b_xor = {32{sub}}^b; 
+    add16 (a[15:0], b_xor[15:0], sub, sum[15:0], w1);
+    add16 (a[31:16], b_xor[31:16], w1, sum[31:16]);
+endmodule
