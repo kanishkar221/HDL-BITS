719309eb390fc7b130de5967fb4a7839142906f8
KANISHKA R <kanishka.r221@gmail.com>
2025-09-13 16:12:50 +0530

    Update 8. Carry Select Adder.v

--- Verilog Language/Modules/8. Carry Select Adder.v
+++ Verilog Language/Modules/8. Carry Select Adder.v
@@ -1,1 +1,19 @@
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+    wire cy1,cy2,cy3;
+    wire [15:0]s1,s2;
+    add16(a[15:0],b[15:0],1'b0,sum[15:0],cy1);
+    add16(a[31:16],b[31:16],1'b0,s1[15:0],cy2);
+    add16(a[31:16],b[31:16],1'b1,s2[15:0],cy3);
+    
+    always @(*) begin
+        if(cy1)
+            sum[31:16] = s2;
+        else
+            sum[31:16] = s1;
+    end
 
+endmodule
