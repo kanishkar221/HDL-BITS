b38d968dac24ed3b8e7f1ee35f72842e8eefb478
KANISHKA R <kanishka.r221@gmail.com>
2025-09-13 16:02:16 +0530

    Update 4. Three Modules.v

--- Verilog Language/Modules/4. Three Modules.v
+++ Verilog Language/Modules/4. Three Modules.v
@@ -1,1 +1,7 @@
+module top_module ( input clk, input d, output q );
+	wire q1,q2;
+    my_dff(.clk(clk), .d(d), .q(q1));
+    my_dff(.clk(clk), .d(q1), .q(q2));
+    my_dff(.clk(clk), .d(q2), .q(q));
+endmodule
 
