9e40d30e193922ac23ed8577eef60335565d9211
KANISHKA R <kanishka.r221@gmail.com>
2025-09-13 16:07:18 +0530

    Update 6. Adder1.v

--- Verilog Language/Modules/6. Adder1.v
+++ Verilog Language/Modules/6. Adder1.v
@@ -1,1 +1,10 @@
-
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+	 wire cy1,cy2;
+    add16(.a(a[15:0]), .b(b[15:0]), .cin(1'b0), .sum(sum[15:0]), .cout(cy1));
+    add16(.a(a[31:16]), .b(b[31:16]), .cin(cy1), .sum(sum[31:16]), .cout(cy2));
+    
+endmodule
