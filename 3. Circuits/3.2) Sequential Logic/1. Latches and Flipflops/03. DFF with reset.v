eccf00e0156acea698e44c4da60e04b0b0b90aa5
KANISHKA R <kanishka.r221@gmail.com>
2025-09-16 21:36:16 +0530

    Update 03. DFF with reset.v

--- 3. Circuits/3.2) Sequential Logic/1. Latches and Flipflops/03. DFF with reset.v
+++ 3. Circuits/3.2) Sequential Logic/1. Latches and Flipflops/03. DFF with reset.v
@@ -1,1 +1,14 @@
+module top_module (
+    input clk,
+    input reset,            
+    input [7:0] d,
+    output [7:0] q
+);
+     always@(posedge clk)begin
+        if(reset)
+            q<=0;
+        else
+            q<=d;
+    end
 
+endmodule
