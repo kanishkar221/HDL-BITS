2646ed5fef87964b399500be314652b5efa5e7cf
KANISHKA R <kanishka.r221@gmail.com>
2025-09-30 22:28:12 +0530

    Update 05. 3-bit LFSR.v

--- 3. Circuits/3.2) Sequential Logic/3. Shift Registers/05. 3-bit LFSR.v
+++ 3. Circuits/3.2) Sequential Logic/3. Shift Registers/05. 3-bit LFSR.v
@@ -1,1 +1,17 @@
+module top_module (
+	input [2:0] SW,      // R
+	input [1:0] KEY,     // L and clk
+	output [2:0] LEDR);  // Q
+	reg [2:0] Q;
+    wire clk = KEY[0];
+    wire L   = KEY[1];
 
+    assign LEDR = Q;
+
+    always @(posedge clk) begin
+        if (L)
+            Q <= SW;                          
+        else
+            Q <= {Q[1] ^ Q[2], Q[0], Q[2]};   
+    end
+endmodule
