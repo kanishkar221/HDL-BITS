1efb4c5b66a282dd9c9b0109fc873553d686523c
KANISHKA R <kanishka.r221@gmail.com>
2025-10-01 21:37:07 +0530

    Update 09. 3-input LUT.v

--- 3. Circuits/3.2) Sequential Logic/3. Shift Registers/09. 3-input LUT.v
+++ 3. Circuits/3.2) Sequential Logic/3. Shift Registers/09. 3-input LUT.v
@@ -1,1 +1,17 @@
+module top_module (
+    input clk,
+    input enable,
+    input S,
+    input A, B, C,
+    output Z ); 
+    
+    reg [7:0] Q;
+    always @(posedge clk) begin  //shift register
+        if (enable)
+            Q <= {Q[6:0], S};
+    end
+    always @(*) begin
+        Z = Q[{A,B,C}];   // 3-8 mux directly index with select lines
+    end
+endmodule
 
