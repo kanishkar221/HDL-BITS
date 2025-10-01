2ef340b4efa1872d9e4284591a8aca2538075c90
KANISHKA R <kanishka.r221@gmail.com>
2025-10-01 21:18:48 +0530

    Update 07. Shift Register.v

--- 3. Circuits/3.2) Sequential Logic/3. Shift Registers/07. Shift Register.v
+++ 3. Circuits/3.2) Sequential Logic/3. Shift Registers/07. Shift Register.v
@@ -1,1 +1,17 @@
-
+module top_module (
+    input clk,
+    input resetn,   // synchronous reset
+    input in,
+    output out);
+    reg [2:0] Q;
+    always @(posedge clk) begin
+        if (!resetn) begin
+            Q   <= 3'b000;
+            out <= 1'b0;
+        end
+        else begin
+            Q   <= {in, Q[2:1]};  // shift left, new bit = in
+            out <= Q[0];          
+        end
+    end
+endmodule
