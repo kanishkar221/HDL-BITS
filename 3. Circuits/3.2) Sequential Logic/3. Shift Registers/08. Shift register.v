85421c2a6d8001b4777840d57c3aec051839af06
KANISHKA R <kanishka.r221@gmail.com>
2025-10-03 14:36:33 +0530

    Update 08. Shift register.v

--- 3. Circuits/3.2) Sequential Logic/3. Shift Registers/08. Shift register.v
+++ 3. Circuits/3.2) Sequential Logic/3. Shift Registers/08. Shift register.v
@@ -1,1 +1,17 @@
-
+module top_module (
+    input [3:0] SW,
+    input [3:0] KEY,
+    output [3:0] LEDR
+); 
+    always @(posedge KEY[0]) begin  
+        if (KEY[2]) begin
+            LEDR <= SW;
+        end 
+        else if (KEY[1]) begin
+            LEDR <= {KEY[3], LEDR[3:1]};
+        end 
+        else begin
+            LEDR <= LEDR;
+        end
+    end
+endmodule
