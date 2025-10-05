0440e82d91dc1da69fda9c91e93899e452b11427
KANISHKA R <kanishka.r221@gmail.com>
2025-10-05 22:26:39 +0530

    Update 01. Simple FSM_1 (asynchronous reset).v

--- 3. Circuits/3.2) Sequential Logic/5. Finite State Machines/01. Simple FSM_1 (asynchronous reset).v
+++ 3. Circuits/3.2) Sequential Logic/5. Finite State Machines/01. Simple FSM_1 (asynchronous reset).v
@@ -1,1 +1,30 @@
+module top_module(
+    input clk,
+    input areset,    // Asynchronous reset to state B
+    input in,
+    output out);//  
 
+    parameter A=0, B=1; 
+    reg state, next_state;
+
+    always @(*) begin
+        case(state)
+        A:if(in == 1)
+			next_state = A;
+          else
+            next_state = B;
+        B:if(in == 1)
+        	next_state = B;
+          else
+            next_state = A;
+        endcase
+    end
+
+    always @(posedge clk, posedge areset) begin
+    	if(areset)
+            state <= B;
+        else
+            state <= next_state;
+    end
+    assign out = (state == B);
+endmodule
