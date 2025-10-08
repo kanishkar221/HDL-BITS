260c0a4dc4d91f285aa84914b030d37d5de168dc
KANISHKA R <kanishka.r221@gmail.com>
2025-10-08 21:45:20 +0530

    Update 02. Simple FSM_1(synchronous reset).v

--- 3. Circuits/3.2) Sequential Logic/5. Finite State Machines/02. Simple FSM_1(synchronous reset).v
+++ 3. Circuits/3.2) Sequential Logic/5. Finite State Machines/02. Simple FSM_1(synchronous reset).v
@@ -1,1 +1,28 @@
+module top_module(clk, reset, in, out);
+    input clk;
+    input reset;    // Synchronous reset to state B
+    input in;
+    output out;//  
+    reg out;
 
+   	parameter A=0, B=1;
+  	reg present_state, next_state;
+
+    always @(posedge clk) begin
+        if (reset) begin  
+            present_state <= B;
+        end 
+        else begin
+            present_state <= next_state;
+        end
+    end
+    
+     always @(*) begin
+        case (present_state)
+            B : next_state = (in == 1) ? B : A;
+            A : next_state = (in == 1) ? A : B;
+        endcase
+    end
+    
+    assign out = (present_state == B);
+endmodule 
