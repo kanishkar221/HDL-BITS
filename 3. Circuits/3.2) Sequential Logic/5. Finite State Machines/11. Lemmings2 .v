3b9f5dceca5157e44e6e8d5173bc615095d957f4
KANISHKA R <kanishka.r221@gmail.com>
2025-10-12 21:36:26 +0530

    Update 11. Lemmings2 .v

--- 3. Circuits/3.2) Sequential Logic/5. Finite State Machines/11. Lemmings2 .v
+++ 3. Circuits/3.2) Sequential Logic/5. Finite State Machines/11. Lemmings2 .v
@@ -1,1 +1,36 @@
+module top_module(
+    input clk,
+    input areset,    // Freshly brainwashed Lemmings walk left.
+    input bump_left,
+    input bump_right,
+    input ground,
+    output walk_left,
+    output walk_right,
+    output aaah );
+    
+	parameter walk_l = 0;
+    parameter walk_r = 1;
+    parameter fall_l = 2;
+    parameter fall_r = 3;
+    reg [1:0] state,next_state;
+    
+    always @ (posedge clk or posedge areset) begin
+        if(areset)
+            state <= walk_l;
+        else 
+            state <= next_state;
+    end
+    
+    always @(*) begin
+        case(state)
+            walk_l : next_state = (ground==0)? fall_l : bump_left? walk_r : walk_l ;
+            walk_r: next_state = (ground==0)? fall_r : bump_right? walk_l : walk_r;
+            fall_l: next_state = ground ? walk_l : fall_l;
+            fall_r: next_state = ground ? walk_r : fall_r;
+        endcase
+    end
 
+    assign walk_left = (state == walk_l);
+    assign walk_right = (state == walk_r);
+    assign aaah = (state == fall_l) || (state == fall_r);
+endmodule
