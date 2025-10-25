898a0f9ff1110ef54cd6f2bc9f788171ae82ec89
KANISHKA R <kanishka.r221@gmail.com>
2025-10-25 15:41:32 +0530

    Implement 4-bit shift register and down counter

--- 3. Circuits/3.3) Building Larger Circuits/02. 4 bit shift register and down counter .v
+++ 3. Circuits/3.3) Building Larger Circuits/02. 4 bit shift register and down counter .v
@@ -1,1 +1,19 @@
-
+module top_module (
+    input clk,
+    input shift_ena,
+    input count_ena,
+    input data,
+    output [3:0] q);
+    
+    always @(posedge clk) begin
+        if(shift_ena)
+            q <= {q[2:0],data};
+        else begin
+            if (count_ena)
+                q <= q - 1'b1;
+            else
+                q <= q;
+        end
+    end
+    
+endmodule
