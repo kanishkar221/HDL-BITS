716d7f7e1b6e1d0f78daf0b62941e9ad371e2183
KANISHKA R <kanishka.r221@gmail.com>
2025-10-25 10:36:07 +0530

    Implement FSM next state logic in Verilog module

--- 3. Circuits/3.2) Sequential Logic/5. Finite State Machines/27. FSM next state logic .v
+++ 3. Circuits/3.2) Sequential Logic/5. Finite State Machines/27. FSM next state logic .v
@@ -1,1 +1,29 @@
-
+module top_module (
+    input [3:1] y,
+    input w,
+    output Y2);
+    
+    parameter A = 0;
+    parameter B = 1;
+    parameter C = 2;
+    parameter D = 3;
+    parameter E = 4;
+    parameter F = 5;
+    
+    reg [2:0] Y;
+    
+    always @(*) begin
+        case(y)
+            A : Y = w ? A:B;
+            B : Y = w ? D:C;
+            C : Y = w ? D:E;
+            D : Y = w ? A:F;
+            E : Y = w ? D:E;
+            F : Y = w ? D:C;
+            default:Y = A;
+            endcase
+        end
+    
+    assign Y2= Y[1];
+    
+endmodule
