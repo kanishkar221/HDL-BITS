73856a05f22d3c2b4788c0a6d9f535c7f0ad4c11
KANISHKA R <kanishka.r221@gmail.com>
2025-10-25 09:45:44 +0530

    Implement FSM with synchronous reset and state transitions

--- 3. Circuits/3.2) Sequential Logic/5. Finite State Machines/25. FSM .v
+++ 3. Circuits/3.2) Sequential Logic/5. Finite State Machines/25. FSM .v
@@ -1,1 +1,37 @@
-
+module top_module (
+    input clk,
+    input reset,   // Synchronous reset
+    input x,
+    output z
+);
+    parameter S0 = 0;
+    parameter S1 = 1;
+    parameter S2 = 2;
+    parameter S3 = 3;
+    parameter S4 = 4;
+    
+    reg [2:0] state, next_state;
+    
+    always @(posedge clk)
+        begin
+            if(reset) 
+                state <= S0;
+            else
+                state <= next_state;
+        end
+    
+    always @(x or state)
+        begin
+            case(state)
+                 S0: next_state <= x? S1: S0;
+                 S1: next_state <= x? S4: S1;
+                 S2: next_state <= x? S1: S2;
+                 S3: next_state <= x? S2: S1;
+                 S4: next_state <= x? S4: S3;
+                default:next_state <= S0;
+            endcase
+        end
+    
+    assign z=(state == S3) || (state == S4);
+    
+endmodule
